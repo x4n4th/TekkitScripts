0e14aef8148dcfbd2d8794b6da582b4d6e68cc99
x4n4th <djlafey@gmail.com>
2013-07-01 21:46:50 -0400

    Need to support multiple bots. Added in progress and added finished function

--- PaveBot/botController.php
+++ PaveBot/botController.php
@@ -4,8 +4,11 @@ if(!$con){
   die('Could not connect: ' . mysqli_error($con));
 }
 
-if( isset($_POST['whatnext']))
-{
+/*Give the bots current locations it will return a chunk that needs
+  attention. Pattern is ChunkID, X, Y, Z in [1,12,34,56]
+*/
+
+if( isset($_POST['whatnext'])){
   $currentLocation = $_POST['whatnext'];
   
   //Attempt to calculate next best location based on current location
@@ -16,19 +19,28 @@ if( isset($_POST['whatnext']))
   //$y = locations[1];
   //$z = locations[2];
   
-  $query = "SELECT * FROM chunks WHERE finished = 0 LIMIT 1;";
+  $query = "SELECT * FROM chunks WHERE finished = 0 AND inProgress = 0 LIMIT 1;";
   
   $result = mysqli_query($con, $query);
   
   if (!$result) {
     die('Invalid query: ' . mysql_error());
   }
+  $id = null
   while($row = mysqli_fetch_array($result))
   {
+    $id = $row['Id'];
+    echo $id . ",";
     echo $row['x'] . ",";
     echo $row['y'] . ",";
     echo $row['z'];
   }
+  
+  mysqli_query($con,"UPDATE chunks SET inProgress = 1 WHERE Id = " . $id);
   mysqli_close($con);
+} elseif ( isset($_POST['finished'])){ //Given a chunk set finished
+  $id = $_POST['finished'];
+  mysqli_query($con,"UPDATE chunks SET inProgress = 0 WHERE Id = " . $id);
+  mysqli_query($con,"UPDATE chunks SET finished = 1 WHERE Id = " . $id);
 }
 ?>
